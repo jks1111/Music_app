7a2b6fda17da7b85dd5e79992539d4285d7123f4
Janhavi <janhavikhushi11singh@gmail.com>
2023-07-12 13:19:21 +0530

    Initial commit

---
+++ songify/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+import Playcard from './components/Playcard';
+function App() {
+  return (
+    <div className="App">
+      This is an app.
+      <Playcard/>
+    </div> 
+  );
+}
+
+export default App;
---
+++ songify/src/components/Playcard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Playcard() {
+  return (
+    <div className="Playcard"> Testing</div>
+  )
+}
+
+export default Playcard
