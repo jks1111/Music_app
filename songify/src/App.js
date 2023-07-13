2818eed6495970fe1b25689c5e25efc885c5700f
Janhavi <janhavikhushi11singh@gmail.com>
2023-07-13 16:32:10 +0530

    added sample.jason

--- songify/src/App.js
+++ songify/src/App.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css';
+import Records from './data/sample.json'
 import Playcard from './components/Playcard';
 function App() {
+  const [songs] = useState([
+    {
+      album: "Tu Jhoothi Main Makkaar",
+    url: "https://open.spotify.com/album/1t1gWslYejaaqicumEbdKU",
+    image: "https://i.scdn.co/image/ab67616d0000b27360098f31391483986de30b66",
+    id: "1t1gWslYejaaqicumEbdKU",
+    artist: "Pritam"
+    },
+    {
+      name: "Tere Pyaar Mein",
+        id: "7Myx7fMLEdkBrkMGK0zhZp",
+        url: "https://open.spotify.com/track/7Myx7fMLEdkBrkMGK0zhZp",
+        image: "https://p.scdn.co/mp3-preview/934085d1005df2d8d0e94833795f9a155dd77d64?cid=0b297fa8a249464ba34f5861d4140e58",
+        duration_ms: 266094,
+        artist: "Pritam, Arijit Singh, Amitabh Bhattacharya, Nikhita Gandhi"
+    }
+  ])
+
+  const [CurrentSongIndex , setCurrentSongIndex] = useState(0);
+  const [NextSongIndex , setNextSongIndex] = useState(0);
+
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if(CurrentSongIndex + 1 > songs.length - 1){
+        return 0 ;
+      }
+     else {
+      return CurrentSongIndex + 1 ;
+     }
+    })
+  }, [ CurrentSongIndex , songs.length] )
   return (
     <div className="App">
       This is an app.
-      <Playcard/>
+      <Playcard CurrentSongIndex = { CurrentSongIndex} setCurrentSongIndex = { setCurrentSongIndex} 
+       NextSongIndex = { NextSongIndex} setNextSongIndex = { setNextSongIndex} songs = { songs } />
     </div> 
   );
 }
--- songify/src/components/Playcard.js
+++ songify/src/components/Playcard.js
@@ -1,23 +1,67 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Details from './Details'
 import Control from './Control'
 
-function Playcard() {
+function Playcard( props) {
+  
+  const audioE1 = useRef(null)
+  const [isPlaying,setisPlaying] = useEffect(false)
+
+  useEffect(( ) => {
+    if(isPlaying){
+      audioE1.current.play()
+    }
+    else{
+      audioE1.current.pause()
+    }
+  })
+  
+  const skipSong = (fowards = true ) => {
+    if(fowards){
+      props.setCurrentSongIndex(() => {
+        let temp = props.CurrentSongIndex;
+        temp++;
+
+        if(temp > props.songs.length - 1){
+          temp = 0 ;
+        }
+
+        return temp;
+      });
+    }
+
+    else {
+      props.setCurrentSongIndex(() => {
+        let temp = props.CurrentSongIndex;
+        temp--;
+
+        if(temp < 0 ){
+          temp = props.songs.length - 1 ;
+        }
+
+        return temp;
+      });
+    }
+  };
   return (
     <div className="Playcard"> 
     <h3>Playing Now</h3>
-    <Details/>
-    <Control/>
+    <Details song = { props.songs[props.CurrentSongIndex] } />
+    <Control isPlaying = { isPlaying} setisPlaying = { setisPlaying } skipSong = { skipSong } />
     <audio className='Playcard_audio'
-            src=""
+            src={props.songs[props.CurrentSongIndex].url}
+            ref = { audioE1}
             controls>
 
     </audio>
 
     <p>
-        Next Up: { " " }
+        Next Up: 
 
-        <span> Next Song Here</span>
+        <span> { props.songs[props.CurrentSongIndex].name} by { " " } 
+        { props.songs[props.CurrentSongIndex].artist }
+
+        </span>
     </p>
     </div>
   )
